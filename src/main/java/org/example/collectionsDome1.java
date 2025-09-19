c8119e3bff51e9c0aa9cdcd0daff22f220c1a019
LXQ <1810742249@qq.com>
2025-09-19 22:17:31 +0800

    aaa

---
+++ src/main/java/org/example/collectionsDome1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class collectionsDome1 {
+
+    public static void main(String[] args) {
+      new Thread(()-> System.out.println("nihao")).start();
+
+    }
+
+
+}
